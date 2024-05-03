eb095ace2442884adb5837c91f53b37e479a00b3
Luana <luana.smatos01@gmail.com>
2024-05-03 18:28:33 -0300

    Terminando página sobre

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <section>
+      <section className="home">
         <div className="texto">
           <h1 className="titulo">Beleza Dourada,<br/>Brilho que Encanta.</h1>
           <p>Nossos cosméticos premium são projetados para realçar sua beleza natural e trazer brilho ao seu dia a dia. Da maquiagem ao cuidado com a pele, cada produto é cuidadosamente selecionado para proporcionar a você a qualidade que merece. Transforme sua rotina de beleza com a Rosa Dourada e brilhe com confiança.</p>
--- src/app/sobre/page.tsx
+++ src/app/sobre/page.tsx
@@ -1,24 +1,15 @@
-import './styles.css';
+import './sobre.css';
+import Image from 'next/image';
 
 export default function Sobre() {
     return (
         <>
-            <section>
-                <h1 className='titulo'>Nossa História</h1>
-                <div className='texto'>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Sint veritatis, magnam iure voluptas molestias, maxime tenetur
-                        ipsum omnis maiores, at modi ullam aliquam aperiam veniam unde ut dolorem delectus hic!</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Sint veritatis, magnam iure voluptas molestias, maxime tenetur
-                        ipsum omnis maiores, at modi ullam aliquam aperiam veniam unde ut dolorem delectus hic!</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Sint veritatis, magnam iure voluptas molestias, maxime tenetur
-                        ipsum omnis maiores, at modi ullam aliquam aperiam veniam unde ut dolorem delectus hic!</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Sint veritatis, magnam iure voluptas molestias, maxime tenetur
-                        ipsum omnis maiores, at modi ullam aliquam aperiam veniam unde ut dolorem delectus hic!</p>
+            <section className='sobre'>
+                <div className='sobre-texto'>
+                    <h1 className='titulo'>Sobre nós</h1>
+                    <p>A beleza e auto-cuidado devem ser para todos. Nossa missão é oferecer produtos de alta qualidade que ajudem você a se sentir bem consigo mesmo. Queremos que cada compra em nossa loja seja uma experiência positiva. Seja para maquiagem, cuidados com a pele ou cabelos, estamos aqui para ajudar você a brilhar.</p>
                 </div>
+                <Image src="/5.png" alt='pessoas' width={700} height={300}></Image>
             </section>
         </>
     );
